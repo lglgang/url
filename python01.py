14313d1fa5bff2dfc99deb30c42201d725aa4efb
ligang <lglgang@126.com>
2022-09-18 11:11:21 +0800

    Update python01.py

--- python01.py
+++ python01.py
@@ -1,1 +1,8 @@
 #this is my python code 
+#这个是读取编码器的代码，使用树莓派
+from ctypes import *
+encoder=cdll.LoadLibrary("/home/pi/./ls7366M0.so")
+encoder.init_encoder2()
+while 1:
+    xx=encoder.get_counter2()
+    print(xx)
